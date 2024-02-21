96701685393ec19fd1cc29704fc5a88a7543c792
ManiacsSoftware <maniacssoftware@gmail.com>
2024-02-21 13:22:20 +0100

    Animated stars fixed

--- backgrounds/animated-stars.js
+++ backgrounds/animated-stars.js
@@ -1,102 +1,108 @@
-"use strict";
 
-var canvas = document.getElementById('canvas'),
-  ctx = canvas.getContext('2d'),
-  w = canvas.width = window.innerWidth,
-  h = canvas.height = window.innerHeight,
-    
-  hue = 235,
-  stars = [],
-  count = 0,
-  maxStars = 5000;
 
-// Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
-// Cache gradient
-var canvas2 = document.createElement('canvas'),
-    ctx2 = canvas2.getContext('2d');
-    canvas2.width = 100;
-    canvas2.height = 100;
-var half = canvas2.width/2,
-    gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-    gradient2.addColorStop(0.025, '#fff');
-    gradient2.addColorStop(0.1, 'hsl(' + hue + ', 15%, 25%)');
-    gradient2.addColorStop(0.25, 'hsl(' + hue + ', 10%, 5%)');
-    gradient2.addColorStop(1, 'transparent');
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
 
-    ctx2.fillStyle = gradient2;
-    ctx2.beginPath();
-    ctx2.arc(half, half, half, 0, Math.PI * 2);
-    ctx2.fill();
+        var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+        w = canvas.width = window.innerWidth,
+        h = canvas.height = window.innerHeight,
 
-// End cache
+        hue = 235,
+        stars = [],
+        count = 0,
+        maxStars = 5000;
 
-function random(min, max) {
-  if (arguments.length < 2) {
-    max = min;
-    min = 0;
-  }
-  
-  if (min > max) {
-    var hold = max;
-    max = min;
-    min = hold;
-  }
+        // Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
+        // Cache gradient
+        var canvas2 = document.createElement('canvas'),
+        ctx2 = canvas2.getContext('2d');
+        canvas2.width = 100;
+        canvas2.height = 100;
+        var half = canvas2.width / 2,
+        gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+        gradient2.addColorStop(0.025, '#fff');
+        gradient2.addColorStop(0.1, 'hsl(' + hue + ', 15%, 25%)');
+        gradient2.addColorStop(0.25, 'hsl(' + hue + ', 10%, 5%)');
+        gradient2.addColorStop(1, 'transparent');
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+        ctx2.fillStyle = gradient2;
+        ctx2.beginPath();
+        ctx2.arc(half, half, half, 0, Math.PI * 2);
+        ctx2.fill();
 
-function maxOrbit(x,y) {
-  var max = Math.max(x,y),
-      diameter = Math.round(Math.sqrt(max*max + max*max));
-  return diameter/2;
-}
+        // End cache
 
-var Star = function() {
+        function random(min, max) {
+            if (arguments.length < 2) {
+                max = min;
+                min = 0;
+            }
 
-  this.orbitRadius = random(maxOrbit(w,h));
-  this.radius = random(100, this.orbitRadius) / 15;// star size min/max
-  this.orbitX = w / 2;
-  this.orbitY = h / 2;
-  this.timePassed = random(0, maxStars);
-  this.speed = random(this.orbitRadius) / 1500000; // Rot speed
-  this.alpha = random(2, 100) / 50; // overal brightness
+            if (min > max) {
+                var hold = max;
+                max = min;
+                min = hold;
+            }
 
-  count++;
-  stars[count] = this;
-}
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-Star.prototype.draw = function() {
-  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
-      y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-      twinkle = random(20); // Twinkle rate
+        function maxOrbit(x, y) {
+            var max = Math.max(x, y),
+            diameter = Math.round(Math.sqrt(max * max + max * max));
+            return diameter / 2;
+        }
 
-  if (twinkle === 1 && this.alpha > 0) {
-    this.alpha -= 0.15;
-  } else if (twinkle === 2 && this.alpha < 1) {
-    this.alpha += 0.15;
-  }
+        var Star = function () {
 
-  ctx.globalAlpha = this.alpha;
-    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
-  this.timePassed += this.speed;
-}
+            this.orbitRadius = random(maxOrbit(w, h));
+            this.radius = random(100, this.orbitRadius) / 15; // star size min/max
+            this.orbitX = w / 2;
+            this.orbitY = h / 2;
+            this.timePassed = random(0, maxStars);
+            this.speed = random(this.orbitRadius) / 1500000; // Rot speed
+            this.alpha = random(2, 100) / 50; // overal brightness
 
-for (var i = 0; i < maxStars; i++) {
-  new Star();
-}
+            count++;
+            stars[count] = this;
+        }
 
-function animation() {
-    ctx.globalCompositeOperation = 'source-over';
-    ctx.globalAlpha = 0.4; // BLUR
-    ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
-    ctx.fillRect(0, 0, w, h)
-  
-  ctx.globalCompositeOperation = 'lighter';
-  for (var i = 1, l = stars.length; i < l; i++) {
-    stars[i].draw();
-  };  
-  
-  window.requestAnimationFrame(animation);
-}
+        Star.prototype.draw = function () {
+            var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+            y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+            twinkle = random(20); // Twinkle rate
 
-animation();
+            if (twinkle === 1 && this.alpha > 0) {
+                this.alpha -= 0.15;
+            } else if (twinkle === 2 && this.alpha < 1) {
+                this.alpha += 0.15;
+            }
+
+            ctx.globalAlpha = this.alpha;
+            ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+            this.timePassed += this.speed;
+        }
+
+        for (var i = 0; i < maxStars; i++) {
+            new Star();
+        }
+
+        function animation() {
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.globalAlpha = 0.4; // BLUR
+            ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
+            ctx.fillRect(0, 0, w, h)
+
+            ctx.globalCompositeOperation = 'lighter';
+            for (var i = 1, l = stars.length; i < l; i++) {
+                stars[i].draw();
+            };
+
+            window.requestAnimationFrame(animation);
+        }
+
+        animation();
+    });
+})();
