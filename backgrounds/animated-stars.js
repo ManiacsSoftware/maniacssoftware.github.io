d8fbd0499efbecb5db0e1859fc7c007926edcf95
ManiacsSoftware <maniacssoftware@gmail.com>
2024-02-21 12:38:30 +0100

    Added animated stars theme

--- backgrounds/animated-stars.js
+++ backgrounds/animated-stars.js
@@ -8,7 +8,7 @@ var canvas = document.getElementById('canvas'),
   hue = 235,
   stars = [],
   count = 0,
-  maxStars = 5400;
+  maxStars = 5000;
 
 // Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
 // Cache gradient
@@ -19,7 +19,7 @@ var canvas2 = document.createElement('canvas'),
 var half = canvas2.width/2,
     gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
     gradient2.addColorStop(0.025, '#fff');
-    gradient2.addColorStop(0.1, 'hsl(' + hue + ', 30%, 25%)');
+    gradient2.addColorStop(0.1, 'hsl(' + hue + ', 15%, 25%)');
     gradient2.addColorStop(0.25, 'hsl(' + hue + ', 10%, 5%)');
     gradient2.addColorStop(1, 'transparent');
 
@@ -54,11 +54,11 @@ function maxOrbit(x,y) {
 var Star = function() {
 
   this.orbitRadius = random(maxOrbit(w,h));
-  this.radius = random(30, this.orbitRadius) / 15;// star size
+  this.radius = random(100, this.orbitRadius) / 15;// star size min/max
   this.orbitX = w / 2;
   this.orbitY = h / 2;
   this.timePassed = random(0, maxStars);
-  this.speed = random(this.orbitRadius) / 2000000; // Rot speed
+  this.speed = random(this.orbitRadius) / 1500000; // Rot speed
   this.alpha = random(2, 100) / 50; // overal brightness
 
   count++;
@@ -68,7 +68,7 @@ var Star = function() {
 Star.prototype.draw = function() {
   var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
       y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-      twinkle = random(50); // Twinkle rate
+      twinkle = random(20); // Twinkle rate
 
   if (twinkle === 1 && this.alpha > 0) {
     this.alpha -= 0.15;
