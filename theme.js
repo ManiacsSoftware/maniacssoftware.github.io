6bc676a573b1e15bf0561920ee033567f0c316d8
ManiacsSoftware <maniacssoftware@gmail.com>
2024-09-05 17:35:19 +0200

    Update GIVING feature!

--- theme.js
+++ theme.js
@@ -1,5 +1,23 @@
 var loadedBackground = "black";
 var versesOnBlack = null;
+var onBlack;
+
+var backgroundElement = document.getElementById('background');
+
+backgroundElement.onload = function(event){
+	console.log("Background on load call! [" + event.target + "]");
+	if(versesOnBlack) {
+		disableBackground();
+	} else {
+		enableBackground();
+		document.querySelector("button[aria-label='Play']")
+		console.log(document.querySelector("button[aria-label='Play']"));
+		
+		if(event.target.querySelector("button[aria-label='Play']")) {
+			event.target.querySelector("button[aria-label='Play']").click();
+		}
+	}
+};
 
 function disableBackground() {
 	backgroundDisabled = true;
@@ -21,18 +39,20 @@ function loadBackground(backgroundName, onBlack) {
 		var boolOnBlack = onBlack === "true";
 		if(versesOnBlack != boolOnBlack) {
 			versesOnBlack = boolOnBlack;
-			if(boolOnBlack) {
-				disableBackground();
-			} else {
-				enableBackground();
-			}
 		}
 	}
 	
 	if(loadedBackground !== backgroundName) {
 		loadedBackground = backgroundName;
-		document.getElementById('background').src = base + 'backgrounds/' + backgroundName + '.html';
-		document.getElementById('templateStyle').href =  base + 'backgrounds/' + backgroundName + '.css';
+		disableBackground();
+		
+		if(backgroundName.startsWith('http')) {
+			document.getElementById('background').src = backgroundName;
+			// document.getElementById('templateStyle').href =  base +	"backgrounds/blank.css"; // Should we clear the theme?
+		} else {
+			document.getElementById('background').src = base + 'backgrounds/' + backgroundName + '.html';
+			document.getElementById('templateStyle').href =  base + 'backgrounds/' + backgroundName + '.css';
+		}
 	}
 }
 
